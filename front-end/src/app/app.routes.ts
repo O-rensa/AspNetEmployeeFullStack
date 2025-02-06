c434739331c6265db6062dd7cf24d9eb26f4ff92
o-rensa <131181504+O-rensa@users.noreply.github.com>
2025-02-06 22:00:45 +0800

    Added pages

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { EmployeesComponent } from './main/Employee/pages/employees/employees.component';
+import { CreateOrEditEmployeeComponent } from './main/Employee/pages/create-or-edit-employee/create-or-edit-employee.component';
+import { ViewEmployeeComponent } from './main/Employee/pages/view-employee/view-employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "main/employee",
+        component: EmployeesComponent
+    },
+    {
+        path: "main/employee/createOrEdit",
+        component: CreateOrEditEmployeeComponent,
+    },
+    {
+        path: "main/employee/view",
+        component: ViewEmployeeComponent,
+    },
+    {
+        path: "**",
+        redirectTo: "main/employee",
+        pathMatch: "full",
+    }
+];
---
+++ front-end/src/app/main/Employee/pages/create-or-edit-employee/create-or-edit-employee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-or-edit-employee',
+  imports: [],
+  templateUrl: './create-or-edit-employee.component.html',
+  styleUrl: './create-or-edit-employee.component.css'
+})
+export class CreateOrEditEmployeeComponent {
+
+}
